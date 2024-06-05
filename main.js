cc78880c7b6fa7c9fed0a6a915d1875d29634f56
erum abbas <eruma6843@gmail.com>
2024-06-05 14:56:03 +0500

    fourteen assignment

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Question#14
+//Define array of guest
+let guestlist = ["Hina", "Salma", "Maria"];
+//Invite each guest for dinner
+guestlist.forEach(guest => {
+    console.log(`Dear ${guest},you are cordially invited to dinner`);
+});
---
+++ main.ts
@@ -0,0 +1,9 @@
+//Question#14
+//Define array of guest
+let guestlist:string[]= ["Hina","Salma","Maria"];
+    
+ //Invite each guest for dinner
+ guestlist.forEach(guest =>{
+    console.log(`Dear ${guest},you are cordially invited to dinner`);
+    
+ });   
